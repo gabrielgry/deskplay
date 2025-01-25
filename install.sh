1473c7576aa94ac6e5bd7408d8bd5615422752e0
Gabriel Grysuk <gabrielgrysuk@gmail.com>
2025-01-25 18:03:59 -0300

    Add execution permission to deskplay.sh

--- install.sh
+++ install.sh
@@ -7,5 +7,7 @@ rm -rf ~/.local/share/deskplay
 # cp -r ./* ~/.local/share/deskplay/
 git clone https://github.com/gabrielgry/deskplay.git ~/.local/share/deskplay
 
+chmod 744 ~/.local/share/deskplay/deskplay.sh
+
 install -d ~/.local/bin
 ln -sf ~/.local/share/deskplay/deskplay.sh ~/.local/bin/deskplay
