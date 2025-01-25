0add9afe801b7facd8a0ac881d57d54166cf10b1
Gabriel Grysuk <gabrielgrysuk@gmail.com>
2025-01-25 18:07:06 -0300

    Modify the remote origin url to use SSH after cloning the repository

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ rm -rf ~/.local/share/deskplay
 # mkdir ~/.local/share/deskplay
 # cp -r ./* ~/.local/share/deskplay/
 git clone https://github.com/gabrielgry/deskplay.git ~/.local/share/deskplay
+git -C ~/.local/share/deskplay remote set-url origin git@github.com:gabrielgry/deskplay.git
 
 chmod 744 ~/.local/share/deskplay/deskplay.sh
 
