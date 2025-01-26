139f950fab9f337229ee59d16c48af0b826c5625
Gabriel Grysuk <gabrielgrysuk@gmail.com>
2025-01-25 23:13:38 -0300

    Remove flatpaks and add qBitorrent dark made fix

--- tasks/applications/flatpak.sh
+++ tasks/applications/flatpak.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 set -euo pipefail
 
+# Add remotes
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak remote-modify --no-filter --enable flathub
 
+# Remove unused Flatpaks
+flatpaks_to_remove=(
+  "org.gnome.Weather"
+  "com.google.Chrome"
+  "org.fedoraproject.MediaWriter"
+  "org.gnome.Calendar"
+  "org.gnome.Connections"
+  "org.gnome.Contacts"
+  "org.gnome.Maps"
+  "org.gnome.clocks"
+)
+
+for app in "${flatpaks_to_remove[@]}"; do
+  if flatpak list --columns=application | grep -w "$app"; then
+    flatpak uninstall flathub "$app" -y --noninteractive --delete-data
+  fi
+done
+
+# Reinstall Flatpaks from the Fedora remote to the Flathub remote
 if flatpak remotes --columns=name | grep -qw "fedora"; then
   apps="$(flatpak list --app-runtime=org.fedoraproject.Platform --columns=application | tail -n +1)"
   if [[ -n "$apps" ]]; then
@@ -12,7 +32,8 @@ if flatpak remotes --columns=name | grep -qw "fedora"; then
   sudo bash -c "yes | flatpak remote-delete fedora"
 fi
 
-flatpak_apps=(
+# Install Flatpaks
+flatpaks_to_install=(
   "com.spotify.Client"
   "ca.desrt.dconf-editor"
   "org.qbittorrent.qBittorrent"
@@ -21,8 +42,11 @@ flatpak_apps=(
   "org.gnome.Extensions"
 )
 
-for app in "${flatpak_apps[@]}"; do
+for app in "${flatpaks_to_install[@]}"; do
   if ! flatpak list --columns=application | grep -w "$app"; then
-    flatpak install flathub "$app" -y
+    flatpak install flathub "$app" -y --noninteractive
   fi
 done
+
+#Fix qBittorrent dark theme
+sudo flatpak override --env=GTK_THEME=Adwaita:dark org.qbittorrent.qBittorrent
